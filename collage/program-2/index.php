82c6c45435783dbcce13b03d37224ebc5f837d7f
Nishanth <innocentnishanth@gmail.com>
2022-03-28 15:59:17 +0530

    index file added

---
+++ collage/program-2/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Super Market</title>
+    <style>
+        h1{
+            color: red;
+            font-size:40px;
+        }
+        h3{
+            color: blue;
+            font-size:30px;
+        }
+        a{
+            text-decoration:none;
+            font-size:25px;
+        }
+    </style>
+</head>
+<body bgcolor="lightgrey">
+        <center><h1>Sri Kannan Departmental Store</h1></center>
+        <h2>Erode - Sathy  main road</h2>
+        <h3>Gobichettipalayam 638 402</h3>
+        <a href="fruits.php">Fruits</a>
+</body>
+</html>
