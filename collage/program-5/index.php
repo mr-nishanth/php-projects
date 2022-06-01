9d8c52c056cdad21ee3a7be6f1989f23645e9597
Nishanth <innocentnishanth@gmail.com>
2022-06-01 20:00:15 +0530

    file renamed

---
+++ collage/program-5/index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>Bakery</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1> Medical Shop </h1>
+    <div class="container">
+        <h3> Medical Shop </h3>
+        <h3>JV Pharmacy</h3>
+        <h4>Phone ID : 9876543210</h4>
+        <h4>Cutchert Street-Sathy main road Gobi-638453.</h4>
+        <h4>Website: jvpharmacy.com</h4>
+        <div class="btn-container">
+            <a class="button" href="snack.php">Snack</a> <br>
+            <a class="button" href="juice.php">Juice</a> <br>
+            <a class="button" href="ice.php">Ice</a> <br>
+        </div>
+    </div>
+</body>
+
+</html>
